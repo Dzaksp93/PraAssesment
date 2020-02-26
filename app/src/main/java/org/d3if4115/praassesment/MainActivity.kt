b3e79819ced9a7bc0e7309cbdfa3275ab3e09980
Dzaki <atepbulbul@gmail.com>
2020-02-26 15:48:43 +0700

    initial Commit

---
+++ app/src/main/java/org/d3if4115/praassesment/MainActivity.kt
@@ -0,0 +1,23 @@
+package org.d3if4115.praassesment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = DataBindingUtil.setContentView<org.d3if4115.praassesment.databinding.ActivityMainBinding>(this,R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
+    }
+
+}
